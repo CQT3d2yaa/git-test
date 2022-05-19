44c9181bf708f4eb5c817377c5f9469e9a4373bc
TaoTao <Tao@atguigu.com>
2022-05-19 18:36:15 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @designer ks&taotao
+ * @create 2022-05-19 18:20
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
